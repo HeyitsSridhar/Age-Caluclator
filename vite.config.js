f404f0bbf8aa2fbceed8360ea584d8b6809e95ab
Sridhar N <sridhar,mca.sri@gmail.com>
2026-07-16 17:00:57 +0530

    Use vite-plugin-singlefile to inline CSS and JS for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Age-Caluclator/',
   publicDir: 'public',
+  plugins: [viteSingleFile()],
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    brotliSize: false,
+  },
 });
